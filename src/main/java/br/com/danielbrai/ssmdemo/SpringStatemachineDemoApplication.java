d3e9585053a14ee303b6c90307a08a995808cbb7
danielbrai <danielbrai@outlook.com>
2023-10-11 07:15:14 -0300

    build: bootstrap do projeto

---
+++ src/main/java/br/com/danielbrai/ssmdemo/SpringStatemachineDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielbrai.ssmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStatemachineDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStatemachineDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/danielbrai/ssmdemo/SpringStatemachineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielbrai.ssmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStatemachineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
