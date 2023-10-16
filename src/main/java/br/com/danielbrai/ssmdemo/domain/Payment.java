5db67b5258ec22bb5034433f03967cae089afe74
danielbrai <danielbrai@outlook.com>
2023-10-16 07:18:40 -0300

    fix: corrige a forma de gerar o id do pagamento

--- src/main/java/br/com/danielbrai/ssmdemo/domain/Payment.java
+++ src/main/java/br/com/danielbrai/ssmdemo/domain/Payment.java
@@ -1,9 +1,6 @@
 package br.com.danielbrai.ssmdemo.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,6 +16,7 @@ import java.math.BigDecimal;
 public class Payment {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
